2dc204a55eed11c0d3885996c5995762911f5720
Daniel <bones2995@hotmail.com>
2017-09-25 18:36:42 -0500

    Movimiento de planetas

---
+++ Doss Plataform/Assets/Scripts/rotate.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotate : MonoBehaviour {
+
+	public GameObject [] planetas;
+	private float speed;	
+	private int i;
+	void Start(){
+		speed = 5;
+	} 
+
+	void Update () {
+		for(i =0; i<12;i++){
+			planetas[i].transform.Rotate(Vector3.up, speed * Time.deltaTime);
+		}
+	}
+}
