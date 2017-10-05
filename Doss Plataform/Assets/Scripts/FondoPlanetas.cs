ecbd79c77fc8860be88763c83bf5d0a25270f7ef
Daniel <bones2995@hotmail.com>
2017-10-04 21:10:14 -0500

    Menu de info planeta

---
+++ Doss Plataform/Assets/Scripts/FondoPlanetas.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FondoPlanetas : MonoBehaviour
+{
+
+    private float speed, tileSize;
+
+    private Vector3 startPosition;
+    void Start()
+    {
+        startPosition = gameObject.transform.position;
+        tileSize = 400f;
+        speed = 5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float newPosition = Mathf.Repeat(Time.time * speed, tileSize);
+        gameObject.transform.position = startPosition + Vector3.up * newPosition;
+    }
+}
--- Doss Plataform/Assets/Scripts/MapController.cs
+++ Doss Plataform/Assets/Scripts/MapController.cs
@@ -1,12 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class MapController : MonoBehaviour
 {
 
-    // Use this for initialization
+    private GameObject planetPanel;
+    private Text planetName, planetDescription;
+
     void Start()
     {
+        planetPanel = GameObject.Find("PlanetInfo");
+
+        planetName = GameObject.Find("PlanetInfo").GetComponentInChildren<UnityEngine.UI.Text>();
+        planetDescription = GameObject.Find("PlanetDescription").GetComponent<UnityEngine.UI.Text>();
+        planetPanel.SetActive(false);
+        //planetName.enabled = false;
+        //planetDescription.enabled = false;
 
     }
 
@@ -14,18 +24,111 @@ public class MapController : MonoBehaviour
     void Update()
     {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-		RaycastHit hit;
+        RaycastHit hit;
 
-		Vector3 forward = transform.TransformDirection(Vector3.forward) * 10;
+        Vector3 forward = transform.TransformDirection(Vector3.forward) * 10;
         Debug.DrawRay(transform.position, forward, Color.green);
 
         if (Physics.Raycast(ray, out hit))
         {
-            if (hit.transform.tag == "Planeta")
+            if (hit.transform.name == "Planeta0" && Input.GetMouseButtonDown(0))
             {
-                Debug.Log("This is Planet " + hit.transform.name);
+                planetPanel.SetActive(true);
+                planetName.text = "Numeros 1 - 10";
+                planetDescription.text = "Me pelan la verga todos";
+
             }
-        
+
+            if (hit.transform.name == "Planeta1" && Input.GetMouseButtonDown(0))
+            {
+                planetPanel.SetActive(true);
+                planetName.text = "Sistema Monetario";
+                planetDescription.text = "Me pelan la verga todos";
+
+            }
+
+            if (hit.transform.name == "Planeta2" && Input.GetMouseButtonDown(0))
+            {
+                planetPanel.SetActive(true);
+                planetName.text = "Culichilandia";
+                planetDescription.text = "Me pelan la verga todos";
+
+            }
+
+            if (hit.transform.name == "Planeta3" && Input.GetMouseButtonDown(0))
+            {
+                planetPanel.SetActive(true);
+                planetName.text = "Pastocas";
+                planetDescription.text = "Me pelan la verga todos";
+
+            }
+
+            if (hit.transform.name == "Planeta4" && Input.GetMouseButtonDown(0))
+            {
+                planetPanel.SetActive(true);
+                planetName.text = "Sexo Anal";
+                planetDescription.text = "Me pelan la verga todos";
+
+            }
+
+            if (hit.transform.name == "Planeta5" && Input.GetMouseButtonDown(0))
+            {
+                planetPanel.SetActive(true);
+                planetName.text = "Weed Planet";
+                planetDescription.text = "Me pelan la verga todos";
+
+            }
+
+            if (hit.transform.name == "Planeta6" && Input.GetMouseButtonDown(0))
+            {
+                planetPanel.SetActive(true);
+                planetName.text = "Numeros 1 - 10";
+                planetDescription.text = "Me pelan la verga todos";
+
+            }
+
+            if (hit.transform.name == "Planeta7" && Input.GetMouseButtonDown(0))
+            {
+                planetPanel.SetActive(true);
+                planetName.text = "Numeros 1 - 10";
+                planetDescription.text = "Me pelan la verga todos";
+
+            }
+
+            if (hit.transform.name == "Planeta8" && Input.GetMouseButtonDown(0))
+            {
+                planetPanel.SetActive(true);
+                planetName.text = "Numeros 1 - 10";
+                planetDescription.text = "Me pelan la verga todos";
+
+            }
+
+            if (hit.transform.name == "Planeta9" && Input.GetMouseButtonDown(0))
+            {
+                planetPanel.SetActive(true);
+                planetName.text = "Numeros 1 - 10";
+                planetDescription.text = "Me pelan la verga todos";
+
+            }
+
+            if (hit.transform.name == "Planeta10" && Input.GetMouseButtonDown(0))
+            {
+                planetPanel.SetActive(true);
+                planetName.text = "Numeros 1 - 10";
+                planetDescription.text = "Me pelan la verga todos";
+
+            }
+
+            if (hit.transform.name == "Planeta11" && Input.GetMouseButtonDown(0))
+            {
+                planetPanel.SetActive(true);
+                planetName.text = "Numeros 1 - 10";
+                planetDescription.text = "Me pelan la verga todos";
+
+            }
+
+            
+
         }
     }
 }
