fde0159e3d1ca44a9621b31242bd2e55dd3001d8
AlejandroVS <Alejandro@MacBookProdeAlejandro.local>
2017-10-12 21:32:34 -0500

    switchMascota

---
+++ Doss Plataform/Assets/Scripts/switchMascota.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class switchMascota : MonoBehaviour {
+
+	public Dropdown Grupo;
+	public Sprite leon;
+	public Sprite delfin;
+
+
+	// Use this for initialization
+	void Start () {
+		Grupo.onValueChanged.AddListener(delegate {
+			myDropdownValueChangedHandler(Grupo);
+		});
+	}
+
+	private void myDropdownValueChangedHandler(Dropdown target) {
+		Debug.Log("selected: "+target.value);
+
+		if (target.value==1) {
+			gameObject.GetComponent<Image> ().sprite = leon;
+			Debug.Log ("Se eligio un Leon");
+		}
+		if (target.value==0) {
+			gameObject.GetComponent<Image> ().sprite = delfin;
+			Debug.Log ("Se eligio un Delfin");
+
+		}
+	}
+}
