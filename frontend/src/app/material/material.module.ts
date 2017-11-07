a2574f6b581875ec5743be8519a7d7020c8a6dff
AlejandroVS <Alejandro@MacBookProdeAlejandro.local>
2017-11-06 19:29:01 -0600

    Card con LogIn

--- frontend/src/app/material/material.module.ts
+++ frontend/src/app/material/material.module.ts
@@ -2,17 +2,20 @@ import { NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material';
 import { MatCardModule } from '@angular/material';
 import { MatIconModule } from '@angular/material';
+import {MatInputModule} from '@angular/material/input';
 
 @NgModule({
     imports: [
         MatCardModule,
         MatToolbarModule,
-        MatIconModule
+        MatIconModule,
+        MatInputModule
     ],
     exports: [
         MatCardModule,
         MatToolbarModule,
-        MatIconModule
+        MatIconModule,
+        MatInputModule
     ]
 })
 export class AMaterialModule { }
