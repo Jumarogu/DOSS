79589e8387ac12852592705fbe1ded627191f669
AlejandroVS <Alejandro@MacBookProdeAlejandro.local>
2017-11-08 23:02:41 -0600

    Se completo LogIn

--- frontend/src/app/material/material.module.ts
+++ frontend/src/app/material/material.module.ts
@@ -3,19 +3,25 @@ import { MatToolbarModule } from '@angular/material';
 import { MatCardModule } from '@angular/material';
 import { MatIconModule } from '@angular/material';
 import {MatInputModule} from '@angular/material/input';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
     imports: [
         MatCardModule,
         MatToolbarModule,
         MatIconModule,
-        MatInputModule
+        MatInputModule,
+        MatCheckboxModule,
+        MatButtonModule
     ],
     exports: [
         MatCardModule,
         MatToolbarModule,
         MatIconModule,
-        MatInputModule
+        MatInputModule,
+        MatCheckboxModule,
+        MatButtonModule
     ]
 })
 export class AMaterialModule { }
