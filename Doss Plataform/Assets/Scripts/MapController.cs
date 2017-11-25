ded3c0f5c6786bb6ae655f7cd7d98e7718501bd4
Daniel <bones2995@hotmail.com>
2017-11-24 23:39:40 -0600

    Movimiento de la camara en menu

--- Doss Plataform/Assets/Scripts/MapController.cs
+++ Doss Plataform/Assets/Scripts/MapController.cs
@@ -14,18 +14,34 @@ public class MapController : MonoBehaviour
     public Sprite Lion;
     public Sprite Dolphin;
     private Image mascota;
-
     public Button jugar;
-    void Start()
-    {
+    private Vector3 [] cameraPosition;
+    
+    void Start(){
         planetPanel = GameObject.Find("PlanetInfo");        
 
         planetName = GameObject.Find("PlanetInfo").GetComponentInChildren<UnityEngine.UI.Text>();
         planetDescription = GameObject.Find("PlanetDescription").GetComponent<UnityEngine.UI.Text>();
         playerName = GameObject.Find("NamePanel").GetComponentInChildren<UnityEngine.UI.Text>();
         planetPanel.SetActive(false);
-        //planetName.enabled = false;
-        //planetDescription.enabled = false;
+        
+
+        jugar.onClick.AddListener(entrarJuego);
+        
+        cameraPosition = new Vector3 [12];
+        cameraPosition[0] = new Vector3(14.5f,-7.5f,-44.5f);
+        cameraPosition[1] = new Vector3(16f,62.5f,-20f);
+        cameraPosition[2] = new Vector3(75f,35f,-20f);
+        cameraPosition[3] = new Vector3(140f,72f,-36f);
+        cameraPosition[4] = new Vector3(88f,-46f,-35f);
+        cameraPosition[5] = new Vector3(150f,-103f,-17f);
+        cameraPosition[6] = new Vector3(-41f,-68f,-17f);
+        cameraPosition[7] = new Vector3(20f,-98f,-27f);
+        cameraPosition[8] = new Vector3(-105f,-70f,-40f);
+        //cameraPosition[9] = new Vector3(,,);
+        //cameraPosition[10] = new Vector3(,,);
+        cameraPosition[11] = transform.position;
+
         cookie = GameObject.Find("Cookies");
         Dictionary<string,string> cook = cookie.GetComponent<sesion>().getcookie();
         playerName.text = cook["nombres"] + " " + cook["apellidos"];
@@ -39,14 +55,13 @@ public class MapController : MonoBehaviour
         if(cook["grupo"] == "b"){
             mascota.sprite = Lion;
         }
-        
 
-        jugar.onClick.AddListener(entrarJuego);
+        
     }
 
     void entrarJuego(){
-        SceneManager.LoadScene("Nivel0");
-        //Debug.Log("Entre al juego ");
+        //SceneManager.LoadScene("Nivel0");
+        Debug.Log("Entre al juego ");
     }
     // Update is called once per frame
     void Update()
@@ -64,7 +79,8 @@ public class MapController : MonoBehaviour
                 planetPanel.SetActive(true);
                 planetName.text = "Numeros 1 - 10";
                 planetDescription.text = "Cuenta las naves y elige en numero correcto para ganar. ";
-
+                this.transform.position = cameraPosition[0];
+                
             }
 
             if (hit.transform.name == "Planeta1" && Input.GetMouseButtonDown(0))
@@ -72,23 +88,23 @@ public class MapController : MonoBehaviour
                 planetPanel.SetActive(true);
                 planetName.text = "Numeracion con letra";
                 planetDescription.text = "Escribe en letra el numero.";
-
+                this.transform.position = cameraPosition[1];
             }
 
             if (hit.transform.name == "Planeta2" && Input.GetMouseButtonDown(0))
             {
                 planetPanel.SetActive(true);
-                planetName.text = "Sumas";
+                planetName.text = "Sumitas";
                 planetDescription.text = "Ejercicios de sumas basicas.";
-
+                this.transform.position = cameraPosition[2];
             }
 
             if (hit.transform.name == "Planeta3" && Input.GetMouseButtonDown(0))
             {
                 planetPanel.SetActive(true);
-                planetName.text = "Sumas 2";
+                planetName.text = "Sumotas";
                 planetDescription.text = "Ejercicios de sumas un poco mas complicadas. ";
-
+                this.transform.position = cameraPosition[3];
             }
 
             if (hit.transform.name == "Planeta4" && Input.GetMouseButtonDown(0))
@@ -96,7 +112,7 @@ public class MapController : MonoBehaviour
                 planetPanel.SetActive(true);
                 planetName.text = "Restitas";
                 planetDescription.text = "Ejercicios de restas sencillos.";
-
+                this.transform.position = cameraPosition[4];
             }
 
             if (hit.transform.name == "Planeta5" && Input.GetMouseButtonDown(0))
@@ -104,7 +120,7 @@ public class MapController : MonoBehaviour
                 planetPanel.SetActive(true);
                 planetName.text = "Restotas";
                 planetDescription.text = "Ejercicios de restas mas grandotas. ";
-
+                this.transform.position = cameraPosition[5];
             }
 
             if (hit.transform.name == "Planeta6" && Input.GetMouseButtonDown(0))
@@ -112,7 +128,7 @@ public class MapController : MonoBehaviour
                 planetPanel.SetActive(true);
                 planetName.text = "Sumas Verticales";
                 planetDescription.text = "Cambio de sumas horizontales a verticales. ";
-
+                this.transform.position = cameraPosition[6];
             }
 
             if (hit.transform.name == "Planeta7" && Input.GetMouseButtonDown(0))
@@ -120,7 +136,7 @@ public class MapController : MonoBehaviour
                 planetPanel.SetActive(true);
                 planetName.text = "Restas Verticales ";
                 planetDescription.text = "Cambio de restas horizontales a verticales. ";
-
+                this.transform.position = cameraPosition[7];
             }
 
             if (hit.transform.name == "Planeta8" && Input.GetMouseButtonDown(0))
@@ -128,7 +144,7 @@ public class MapController : MonoBehaviour
                 planetPanel.SetActive(true);
                 planetName.text = "Sumas y restas. ";
                 planetDescription.text = "Combinacion de sumas y restas";
-
+                this.transform.position = cameraPosition[8];
             }
 
             if (hit.transform.name == "Planeta9" && Input.GetMouseButtonDown(0))
